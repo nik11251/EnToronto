394b27305a9ab058448a7e21ba1a9e8c36071893
nik11251 <64909987+nik11251@users.noreply.github.com>
2024-09-26 18:03:33 -0400

    removed box

--- src/components/Slide/MySlide.tsx
+++ src/components/Slide/MySlide.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { FC } from "react";
 import Grid from "@mui/material/Grid2";
 import MyImage from "../MyImage";
